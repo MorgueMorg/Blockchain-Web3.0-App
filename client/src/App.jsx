993a11a465e41d3131b6cfa8a86b04c917c9ea54
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-08-31 22:50:31 +0600

    updated app.jsx file :octocat:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-};
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
