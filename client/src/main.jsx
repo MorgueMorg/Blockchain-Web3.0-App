af1bd39ebbaed8406a4f9bc9fb021fee47721782
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-05 21:57:14 +0600

    fixed utils and added transaction context :dart:

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import App from "./App";
+import { TransactionsProvider } from "./context/TransactionContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <TransactionsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionsProvider>
+);
