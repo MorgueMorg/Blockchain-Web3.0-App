0d6f0863365f2332e9f8e610ccc3be7d8262fb71
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-05 23:17:01 +0600

    added first drafts for transactions :rocket:

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+import dummyData from "../utils/dummyData";
 
 const Transactions = () => {
-  return (
-    <div>Transactions</div>
-  )
-}
+  const { transactions, currentAccount } = useContext(TransactionContext);
 
-export default Transactions
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">
+            Latest Transactions
+          </h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">
+            Connect your account to see the latest transactions
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Transactions;
