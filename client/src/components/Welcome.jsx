abb50841024b07508eff57a5f29384452d641be8
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-05 23:26:39 +0600

    added account address for card :rocket:

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -92,6 +92,9 @@ const Welcome = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
+                <p className="text-white font-light text-sm">
+                  {currentAccount}
+                </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
                 </p>
