ce45b82c069d56fcaa831a29ce0e854f6c7a12d3
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-05 23:31:19 +0600

    added shorten address :dart:

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,7 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
@@ -93,7 +94,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  {currentAccount}
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
---
+++ client/src/utils/shortenAddress.js
@@ -0,0 +1,2 @@
+export const shortenAddress = (address) =>
+  `${address.slice(0, 5)}...${address.slice(address.length - 4)}`;
