71c61f1efd36c7de0d7c3a5f5b9169a599b176d0
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-06 13:09:20 +0600

    updated hardhat config :octocat:

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,9 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli: {
+      // Your alchemy https link
       url: process.env.URL,
+      // Your deploy key from terminal command (from hardhat)
       accounts: [process.env.ACCOUNTS],
     },
   },
